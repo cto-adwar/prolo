b65a9aad7266965fbdd4cffc39536cda079c2bca
Muhammad Mohsin <nuqtamenu@gmail.com>
2026-03-08 18:36:44 +0500

    Updated The Hero section - Removed the center buttons & input, - moved the input to the bottom with the logos.

--- src/app/[locale]/_components/sections/Hero.tsx
+++ src/app/[locale]/_components/sections/Hero.tsx
@@ -13,56 +13,31 @@ export default function Hero() {
   return (
     <section
       id="#hero"
-      className="sm:py-auto relative z-1 flex w-full flex-col items-center justify-center text-white"
+      className="sm:py-auto relative z-1 flex min-h-[600px] w-full flex-col items-center justify-center text-white"
     >
       {/* Overlay */}
       <div className="absolute inset-0 h-full w-full">
         <HeroLazySlider />
       </div>
 
-      {/* Content */}
-      <div className="prolo-container relative z-2 flex h-auto w-full flex-col items-center justify-between gap-8 pt-23 pb-12 sm:gap-12 md:gap-12">
+      {/* HERO Content */}
+      <div className="relative z-2 flex h-full min-h-[600px] w-full flex-col items-center justify-between gap-8 p-8 pt-23 pb-12 sm:gap-12 md:gap-12">
+        {/* Top - title */}
         <div>
-          {/* Top - title */}
-          <h2 className="mx-auto text-center text-2xl/tight font-bold text-white sm:w-[60%] sm:text-4xl/tight">
+          <h2 className="mx-auto text-center text-3xl/tight font-bold text-white sm:w-[60%] sm:text-4xl/tight">
             {t("hero.title.first")} <span className="">{t("hero.title.mid")}</span>{" "}
             {t("hero.title.last")}
           </h2>
         </div>
 
-        {/* Buttom & Input */}
-        <div className="mt-12 hidden w-full max-w-[800px] sm:block sm:w-[90%]">
-          {/* Button */}
-          <Button
-            text={ctas("createShipment")}
-            href={`/${locale}/create-shipment/`}
-            direction="forward"
-            className="bg-white-hover/15 hover:bg-theme-blue hover:border-theme-blue mb-4 border border-white"
-          />
-          {/* Tracking Input */}
-          <TrackingInput />
-        </div>
-
-        {/* Button */}
-        <div className="flex flex-wrap items-center justify-center gap-2 sm:hidden">
-          <Button
-            text={ctas("createShipment")}
-            href={`/${locale}/create-shipment/`}
-            direction="forward"
-            className="bg-white-hover/15 hover:bg-theme-blue hover:border-theme-blue min-w-[200px] border border-white"
-          />
-          <Button
-            text={ctas("trackShipment")}
-            href={`/${locale}/tracking/`}
-            className="bg-theme-blue hover:bg-blue-hover min-w-[200px]"
-            direction="forward"
-          />
-        </div>
-
-        {/* bottom - description + logos */}
-        <div className="prolo-container flex w-full items-center justify-center md:justify-between">
+        {/* Logos & Input Bar */}
+        <div className="flex w-full flex-col-reverse items-center justify-center gap-4 md:flex-row md:justify-between">
           {/* Hero Logos */}
           <HeroLazyLogos />
+          {/* Tracking Input */}
+          <div className="w-full max-w-[500px]">
+            <TrackingInput />
+          </div>
         </div>
       </div>
     </section>
